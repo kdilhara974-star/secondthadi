849dbdcce8d4733ce14422882fecc1c8c398b600
DARK-CYBER-TECH-OFC <kdilhara974@gmail.com>
2026-02-21 20:50:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ranu&NBFW3YBT#XvxBXxnUQL52ZPMWGXYVFprGCLuR8yrl1V8Gh_CF0QA",
+SESSION_ID: process.env.SESSION_ID || "ranu&8ZEwjBgZ#oIVceIShU7DXkWcEc0tuEcebwnNuEJ-WPfW_rn8O3Ss",
 // add your Session Id (මුලට "ranu&")
 AUTO_RECORDING: process.env.AUTO_RECORDING || "false",
 // make it true for auto recoding 
